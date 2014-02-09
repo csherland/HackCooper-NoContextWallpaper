765eb6fa97082b65ada48d880a41940eb80657fc
OstrichBanger <jonathanost@gmail.com>
2014-02-09 05:40:01 -0500

    change to jpeg and 800x600

--- nocontext/image_get.py
+++ nocontext/image_get.py
@@ -4,9 +4,9 @@ import praw
 porn = ['earthporn','waterporn','skyporn','spaceporn','fireporn','destructionporn','geologyporn','cityporn','villageporn','abandonedporn','infrastructureporn','machineporn','militaryporn','cemeteryporn','architectureporn','animalporn','botanicalporn','humanporn','adrenalineporn','climbingporn','culinaryporn','designporn','albumartporn','metalporn','geekporn','roomporn','instrumentporn','macroporn','microporn','artporn','fractalporn','exposureporn','streetartporn','historyporn','mapporn','newsporn','futureporn','fossilporn']
 
 r = praw.Reddit('hackcooper')
-f = open('photolinks.txt', 'a+')
+f = open('static/js/photolinks.txt', 'a+')
 for sub in porn:
-	submissions = r.get_subreddit(sub).get_top_from_all(limit=10)
+	submissions = r.search('[800x600]', subreddit=sub, sort='top', syntax=None, period='all', limit=10)
 	for x in submissions:
 		exten = str(x.url)
 		exten = exten.lower()
