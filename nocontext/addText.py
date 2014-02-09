e124848aafbfe8a8aa1348a969ad9646b8641e52
OstrichBanger <jonathanost@gmail.com>
2014-02-09 05:09:34 -0500

    changed to front

--- nocontext/addText.py
+++ nocontext/addText.py
@@ -65,11 +65,11 @@ def addText(text, author, imgurl):
 	draw.text((x+1, y+1), author, (0,0,0), font=font)
 	draw.text((x,y), author, (255,255,255), font=font)
 
-	img = img.resize( [int(0.25 * s) for s in img.size] )
+	img = img.resize( [int(0.5 * s) for s in img.size] )
 	
 	output = cStringIO.StringIO()
 	img.save(output, "PNG")
-	img = str(output.getvalue())
+	img = output.getvalue()
 	output.close()
 
 	return img
